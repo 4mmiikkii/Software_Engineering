a7d18c65322a49813273dcebf32923e34a229889
みき <aruarusakura4@gmail.com>
2019-04-19 17:04:29 +0900

    ゆいちゃんだーいすき

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("T TT T T TT");
+		System.out.println("T TT T T TT");
+		System.out.println("どうもーTT兄弟です！");
 	}
 
 }
